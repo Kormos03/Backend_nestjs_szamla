2096b51ead16420f404348ff9689af35653ada7c
Kormos Áron <104300544+Kormos03@users.noreply.github.com>
2024-01-23 15:28:01 +0100

    kesz

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Render } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
 
@@ -7,7 +7,7 @@ export class AppController {
   private readonly prismaService: PrismaService;
   constructor(
     private readonly appService: AppService,
-    prismaService PrismaService
+    prismaService: PrismaService
   ) {
     this.prismaService = prismaService;
   }
@@ -19,14 +19,52 @@ export class AppController {
   }
   @Get('bills')
   listOfBills() {
-    return this.prismaService.
+    return this.prismaService.bills.findMany()
   }
 
-  @Get('bills / id: id')
-  billDetails(@Param(id) id: string) {
-    return this.pri
+  @Get('bills/id/:id')
+  async billDetails(@Param('id') id: string) {
+    try {
+      return await this.prismaService.bills.findFirstOrThrow({
+        where: { id: parseInt(id) }
+      })
+    } catch {
+      throw new NotFoundException('Nincs ilyen ID-jú számla')
+    }
+  }
+
+  @Get('bills/type/:type')
+  billsByType(@Param('type') type: string) {
+    const normalizedType = type.toLowerCase();
+    if (normalizedType === "all") {
+      return this.listOfBills();
+    } else {
+      return this.prismaService.bills.findMany({
+        where: { type: normalizedType },
+      });
+    }
+  }
+
+  @Get('bills/urgent')
+  urgentBills() {
+    return this.prismaService.bills.findFirst({
+      orderBy: {
+        due: 'asc',     //növekvő sorba rendezés
+      }
+    })
+  }
+  @Get('bills/late')
+  lateBills() {
+    return this.prismaService.bills.findMany({
+      where: {
+        due: {
+          lt: new Date()
+        }
+      }
+    })
   }
 }
+
 
 
 
