8497a45aace8ce55fc0fff3ba6ccae434dede378
Kormos Áron <104300544+Kormos03@users.noreply.github.com>
2024-01-23 14:57:10 +0100

    started

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,45 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Param, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private readonly prismaService: PrismaService;
+  constructor(
+    private readonly appService: AppService,
+    prismaService PrismaService
+  ) {
+    this.prismaService = prismaService;
+  }
 
   @Get()
   @Render('index')
   index() {
     return { message: 'Welcome to the homepage' };
   }
+  @Get('bills')
+  listOfBills() {
+    return this.prismaService.
+  }
+
+  @Get('bills / id: id')
+  billDetails(@Param(id) id: string) {
+    return this.pri
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//npx prisma generate, majd npx prisma db pull, majd npx prisma db push
